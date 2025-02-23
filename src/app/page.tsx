6a6ed1fbeb80eb289d7720ec8ac88070f0804e88
sar9484 <batbayarsaruul0@gmail.com>
2025-02-23 23:31:35 +0800

    fasdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
-'use client'
+"use client";
 import { useState } from "react";
+import axios from "axios";
 import { SignUp } from "../components/SignUp";
 import { SignUpStep2 } from "../components/SignUpStep2";
 
 export default function Home() {
-  const [step,setStep] = useState(1)
+  const [step, setStep] = useState(1);
+  const checkSignedUp = async () => {
+    const response = await axios.get("http://localhost:5000/user/signup");
+  };
   return (
     <div className="flex w-screen items-center justify-center py-5 ">
       <div className="flex w-[1300px] items-center justify-center gap-5">
-        {
-          step==1&&<SignUp setStep={setStep}/>
-        }
-        {
-          step==2&&<SignUpStep2 setStep={setStep}/>
-        }
-      <img src="mainImage.png" alt="" />
+        {step == 1 && <SignUp setStep={setStep} />}
+        {step == 2 && <SignUpStep2 setStep={setStep} />}
+        <img src="mainImage.png" alt="" />
       </div>
     </div>
   );
